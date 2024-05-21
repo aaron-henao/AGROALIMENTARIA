b6b0a20ce4a62bba395bbc17d0321a767e4bb5ce
Aaron <110606543+aaron-henao@users.noreply.github.com>
2024-05-20 20:39:46 -0500

    Add files via upload

---
+++ AGROALIMENTARIA/Agroalimentaria/src/Producto.java
@@ -0,0 +1,17 @@
+public class Producto {
+
+    private String caducidad;
+
+    private int lote;
+
+
+    public Producto() {
+    }
+
+    //getters y setters
+    public String getCaducidad() {return caducidad;}
+    public int getLote() {return lote;}
+    public void setCaducidad(String caducidad) {this.caducidad = caducidad;}
+    public void setLote(int lote) {this.lote = lote;}
+
+}
